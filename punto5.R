4a6ee7c61fe007c1e3a627a0a9702c56f7bf1c99
Tavo <gariosp@unal.edu.co>
2018-10-27 14:59:27 -0300

    Adding graphs for k-means cluster analysis for k=2, 6 and 8, for hipparcos and hipparcos scaled

--- punto5.R
+++ punto5.R
@@ -2,17 +2,19 @@ library("FactoMineR")
 library("factoextra")
 library("cluster")
 
+
 #source('3DPlotFunctionsFactoMiner.r')
 library("PerformanceAnalytics")
 chart.Correlation(hipparcos_numeric, histogram = TRUE)
-install.packages('ggcorrplot', dependencies=TRUE, repos='http://cran.rstudio.com/')
+#install.packages('ggcorrplot', dependencies=TRUE, repos='http://cran.rstudio.com/')
 library("ggcorrplot")
 
 
 corr <- round(cor(hipparcos_numeric[, -c(1,9)]), 1)
 ggcorrplot(corr, hc.order = TRUE, lab = TRUE)
 
-library(ggplot2)
+library("ggplot2")
+#install.packages("ggplot2")
 plotmatrix(hip2[, - c(1, 9)])
 library(GGally)
 ggpairs(hipparcos_numeric[, - c(1, 9)], aes(alpha = 0.4))
@@ -28,7 +30,7 @@ hiparscaled <- scale(hip2[, 2:8])
 rownames(hiparscaled) <- hip2$HIP
 
 res.pca <- PCA(hiparscaled, graph = FALSE)
-
+###
 fviz_pca_ind(res.pca, geom = "point", col.ind = hip2$miembro)
 
 #contribucion de variables
@@ -46,9 +48,7 @@ fviz_pca_ind(res.pca, label = "none", habillage = hip2$miembro, addEllipses = TR
 #rgl.viewpoint(zoom = .5)
 #PCABIPlot3D(res.pca)
 
-#kmeans = 2 , hyades y no hyades?
-kmeansresult <- kmeans(hiparscaled, 2, nstart = 25)
-kmeansresult8 <- kmeans(hiparscaled, 8, nstart = 25)
+
 
 
 #####este arma las 4 metricas
@@ -58,7 +58,14 @@ fviz_nbclust(hiparscaled, pam, method = "wss") + geom_vline(xintercept = 9, line
 fviz_nbclust(hiparscaled, pam, method = "silhouette")
 
 
-fviz_cluster(kmeansresult8, data = hiparscaled,
+
+#kmeans = 2 , hyades y no hyades?
+
+kmeansresult <- kmeans(hiparscaled, 2, nstart = 25)
+kmeansresult6 <- kmeans(hiparscaled, 6, nstart = 25)
+kmeansresult8 <- kmeans(hiparscaled, 8, nstart = 25)
+
+fviz_cluster(kmeansresult6, data = hiparscaled,
 #palette = c("#00AFBB", "#FC4E07"), # color palette
 			 #ellipse.type = "t", # Concentration ellipse
 			geom = c("point"),
@@ -67,31 +74,64 @@ fviz_cluster(kmeansresult8, data = hiparscaled,
 			 )
 
 
-ggplot(as.data.frame(hiparscaled), aes(x = Vmag, y = B.V, color = as.factor(kmeansresult8$cluster))) +
+ggplot(as.data.frame(hiparscaled), aes(x = Vmag, y = B.V, color = as.factor(kmeansresult$cluster))) +
     geom_point() + geom_rug()
 
 #print(kmeansresult)
 kmeansresult$size
+kmeansresult6$size
+kmeansresult8$size
+
 kmeansresult$cluster <- as.factor(kmeansresult$cluster)
+kmeansresult6$cluster <- as.factor(kmeansresult6$cluster)
+kmeansresult8$cluster <- as.factor(kmeansresult8$cluster)
 
-fviz_pca_biplot(res.pca, axes = c(1, 2), label = "var", alpha.var = "contrib", habillage = kmeansresult$cluster, repel = T)
-fviz_pca_biplot(res.pca, axes = c(1, 2), label = "var", alpha.var = "contrib", habillage = hip2$miembro, repel = T, addEllipses = TRUE, ellipse.level = 0.95)
 
-fviz_pca_biplot(res.pca, axes = c(2, 3), label = "var", alpha.var = "contrib", habillage = kmeansresult$cluster, repel = T)
-fviz_pca_biplot(res.pca, axes = c(2, 3), label = "var", alpha.var = "contrib", habillage = hip2$miembro, repel = T, addEllipses = TRUE, ellipse.level = 0.95)
 
-fviz_pca_biplot(res.pca, axes = c(1, 3), label = "var", alpha.var = "contrib", habillage = kmeansresult$cluster, repel = T)
-fviz_pca_biplot(res.pca, axes = c(1, 3), label = "var", alpha.var = "contrib", habillage = hip2$miembro, repel = T, addEllipses = TRUE, ellipse.level = 0.95)
+fviz_pca_biplot(res.pca, axes = c(1, 2), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(1, 2), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult6$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(1, 2), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult8$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(1, 2), label = "var", 
+                alpha.var = "contrib", habillage = hip2$miembro, repel = T, 
+                addEllipses = TRUE, ellipse.level = 0.95)
+
+
+fviz_pca_biplot(res.pca, axes = c(1, 3), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(1, 3), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult6$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(1, 3), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult8$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(1, 3), label = "var", 
+                alpha.var = "contrib", habillage = hip2$miembro, repel = T, 
+                addEllipses = TRUE, ellipse.level = 0.95)
+
+fviz_pca_biplot(res.pca, axes = c(2, 3), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(2, 3), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult6$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(2, 3), label = "var", 
+                alpha.var = "contrib", habillage = kmeansresult8$cluster, repel = T)
+fviz_pca_biplot(res.pca, axes = c(2, 3), label = "var", 
+                alpha.var = "contrib", habillage = hip2$miembro, repel = T, 
+                addEllipses = TRUE, ellipse.level = 0.95)
+
+
 
 
 #optima cantidad de k 
 fviz_nbclust(hiparscaled, pam, method = "silhouette") +   theme_classic()
 #8 segun pam medido por silhouette
 
-pam.res <- pam(hiparscaled, 8)
 pam.resk2 <- pam(hiparscaled, 2)
+pam.resk6 <- pam(hiparscaled, 6)
+pam.resk8 <- pam(hiparscaled, 8)
 
-#pam k = 8
+
+#pam k = 2
 fviz_cluster(pam.res,
 			 #palette = c("#00AFBB", "#FC4E07"), # color palette
 			 ellipse.type = "t", # Concentration ellipse
@@ -100,15 +140,22 @@ fviz_cluster(pam.res,
 			 ggtheme = theme_classic()
 			 )
 
-#pam k = 2
-fviz_cluster(pam.resk2,
-			palette = c("#00AFBB", "#FC4E07"), # color palette
+#pam k = 6
+fviz_cluster(pam.resk6,
+			#palette = c("#00AFBB", "#FC4E07"), # color palette
 			 ellipse.type = "t", # Concentration ellipse
 			geom = c("point"),
 			#repel = TRUE, # Avoid label overplotting (slow)
 			 ggtheme = theme_classic()
 			 )
 
+fviz_cluster(pam.resk8,
+             #palette = c("#00AFBB", "#FC4E07"), # color palette
+             ellipse.type = "t", # Concentration ellipse
+             geom = c("point"),
+             #repel = TRUE, # Avoid label overplotting (slow)
+             ggtheme = theme_classic()
+)
 
 fviz_silhouette(silhouette(pam.resk2))
 
